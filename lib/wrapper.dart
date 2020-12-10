8d59de4c5dcbd796feb585fdd4eafb0dd55f09f3
gunyguni <manpkh95@gmail.com>
2020-12-10 23:03:30 +0900

    asdf

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -19,33 +17,33 @@ class Wrapper extends StatelessWidget {
       return LoginPage();
     } else {
       return StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance.collection('user')
-            .doc(user.uid)
-            .snapshots(),
-        builder: (BuildContext context, snapshot) {
-          if (!snapshot.hasData || snapshot.data.data() == null) {
-            //print(snapshot.data.get('phonenum'));
-            return PersonInformation();
-          } else {
-            user.phoneNo = snapshot.data.get('phonenum');
-            print(user.phoneNo);
-            return HomePage();
-          }
-        });
-
-        }
-
-      if (user == null) {
-        return LoginPage();
-        // }
-        // else if (FirebaseFirestore.instance
-        //         .collection('user')
-        //         .where('uid', isEqualTo: user.uid)
-        //         .snapshots() ==
-        //     null) {
-        //   return PersonInformation();
-      } else {
-        return PersonInformation();
-      }
+          stream: FirebaseFirestore.instance
+              .collection('user')
+              .doc(user.uid)
+              .snapshots(),
+          builder: (BuildContext context, snapshot) {
+            if (!snapshot.hasData || snapshot.data.data() == null) {
+              //print(snapshot.data.get('phonenum'));
+              return PersonInformation();
+            } else {
+              user.phoneNo = snapshot.data.get('phonenum');
+              print(user.phoneNo);
+              return HomePage();
+            }
+          });
     }
+
+    if (user == null) {
+      return LoginPage();
+      // }
+      // else if (FirebaseFirestore.instance
+      //         .collection('user')
+      //         .where('uid', isEqualTo: user.uid)
+      //         .snapshots() ==
+      //     null) {
+      //   return PersonInformation();
+    } else {
+      return PersonInformation();
+    }
+  }
 }
