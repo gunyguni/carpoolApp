c4083045577d6342b1acec374a1fe73d270cb533
jaejae-kim <21600134@handong.edu>
2020-12-11 02:20:06 +0900

    profile done

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:handongcarpool/model/user_info.dart';
 import 'package:handongcarpool/screens/login.dart';
+import 'package:handongcarpool/service/auth.dart';
 import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -14,6 +15,7 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   User users = FirebaseAuth.instance.currentUser;
+  AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<TheUser>(context);
@@ -27,7 +29,7 @@ class _ProfilePageState extends State<ProfilePage> {
             icon: Icon(Icons.edit),
             color: Colors.white,
             onPressed: () async {
-              await FirebaseAuth.instance.signOut();
+              await _auth.signOut();
               Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -38,7 +40,7 @@ class _ProfilePageState extends State<ProfilePage> {
             icon: Icon(Icons.exit_to_app),
             color: Colors.white,
             onPressed: () async {
-              await FirebaseAuth.instance.signOut();
+              await _auth.signOut();
               Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -48,46 +50,91 @@ class _ProfilePageState extends State<ProfilePage> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            color: Colors.black,
-            child: Column(
-              children: [
-                 Padding(
-                   padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
-                   child: Container(
-                    color: Colors.black,
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          width: 150,
-                          height: 150,
-                          child: Image(
-                            fit: BoxFit.fill,
-                            image: NetworkImage(users.photoURL.toString()),
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+               Padding(
+                 padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+                 child: Container(
+                  color: Colors.black,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: 25,),
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundImage: NetworkImage(user.url),
+                            ),
+                          SizedBox(width: 20,),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text('학번: '),
+                                  Text(user.stunum, style: TextStyle(fontSize: 20, color: Colors.white),),
+                                ],
+                              ),
+                              SizedBox(height: 20,),
+                              Row(
+                                children: [
+                                  Text('휴대폰: '),
+                                  Text(user.phoneNo, style: TextStyle(fontSize: 20, color: Colors.white),),
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Divider(
+                        color: Colors.grey,
+                        thickness: 2,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text('내가 신청한 카풀목록'),
+                      SizedBox(height: 30,),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 2,
                           ),
                         ),
-                        SizedBox(
-                          height: 120,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text('From handong'),
+                              ],
+                            ),
+                            SizedBox(height: 102,),
+                            Divider(
+                              color: Colors.grey,
+                              thickness: 2,
+                            ),
+                            Row(
+                              children: [
+                                Text('To handong'),
+                              ],
+                            ),
+                            SizedBox(height: 102,),
+                          ],
                         ),
-                        //Text('\<' + user.stunum + '\>', style: TextStyle(fontSize: 20, color: Colors.white),),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Divider(
-                          color: Colors.grey,
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        //Text(user.email, style: TextStyle(color: Colors.white),),
-                        SizedBox(height: 120,),
-                      ],
-                    ),
-                ),
-                 ),
-              ],
-            ),
+                      ),
+                    ],
+                  ),
+              ),
+               ),
+            ],
           ),
         ),
       ),
